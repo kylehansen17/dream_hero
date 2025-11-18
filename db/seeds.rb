0094d9cc7843d7110c7b73a580c82a6b507d600c
Fan Xu <176100259+CodingX-lab@users.noreply.github.com>
2025-11-18 16:53:04 +0900

    refactor: made working seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,127 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-user = User.first || User.create(email: "testboi@sam.com", password: "123456")
-stories = [{
+DEFAULT_TITLE = "untitled"
+
+puts "Cleaning database..."
+# Restaurant.destroy_all
+StoryCharacter.destroy_all
+Character.destroy_all
+Story.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+sam = User.create!(email: "sam@mail.com", password: "123456")
+kyle = User.create!(email: "kyle@mail.com", password: "123456")
+tyrhen = User.create!(email: "tyrhen@mail.com", password: "123456")
+fan = User.create!(email: "fan@mail.com", password: "123456")
+
+# ----------------------------------
+
+hood = Character.create({
+  name: "Little Red Riding Hood",
+  occupation: "wolf hunter",
+  likes: "grandma",
+  user: sam
+})
+
+hoodstory = Story.create({
   name: "Little Red Riding Boot",
   age: 5,
   theme: "adventure",
-  system_prompt: "system_prompt"
-}, {
+  system_prompt: "",
+  user: sam
+})
+
+
+StoryCharacter.create({
+  story: hoodstory,
+  character: hood,
+})
+
+Chat.create({
+  title: DEFAULT_TITLE,
+  story: hoodstory
+})
+
+# ----------------------------------
+
+
+wolf = Character.create({
+  name: "Lupus",
+  occupation: "Granny Eater",
+  likes: "Grannies",
+  user:fan
+})
+
+wonderland = Story.create({
   name: "Sam in Wonderland",
   age: 7,
   theme: "fantasy",
-  system_prompt: "system_prompt"
-}, {
+  system_prompt: "",
+  user: fan
+})
+
+StoryCharacter.create({
+  story: wonderland,
+  character: hood,
+})
+
+Chat.create({
+  title: DEFAULT_TITLE,
+  story: wonderland
+})
+
+# -------------------
+
+hood = Character.create({
+  name: "Little Red Riding Hood",
+  occupation: "wolf hunter",
+  likes: "grandma",
+  user: tyrhen
+})
+
+dino = Story.create({
   name: "Dino's Day Out",
   age: 3,
   theme: "cute",
-  system_prompt: "system_prompt"
-}, {
+  system_prompt: "",
+  user: tyrhen
+})
+
+StoryCharacter.create({
+  story: dino,
+  character: wolf,
+})
+
+Chat.create({
+  title: DEFAULT_TITLE,
+  story: dino
+})
+
+# -------------------
+
+wolf = Character.create({
+  name: "Lupus",
+  occupation: "Granny Eater",
+  likes: "Grannies",
+  user: kyle
+})
+
+
+eldermoor = Story.create({
   name: "Shadows of Eldermoor",
   age: 14,
   theme: "dark",
-  system_prompt: "system_prompt"
-}
-]
-puts "Cleaning the DB"
-stories.each { |s| Story.create!(s.merge(user: user))}
-puts "Created #{Story.count} stories"user = User.first || User.create(email: "testboi@sam.com", password: "123456")
+  system_prompt: "",
+  user: kyle
+})
+
+
+StoryCharacter.create({
+  story: eldermoor,
+  character: wolf,
+})
+
+
+Chat.create({
+  title: DEFAULT_TITLE,
+  story: eldermoor
+})
