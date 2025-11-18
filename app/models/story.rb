72fc8ddc86546eb6659e977ff86a25e98ff88ed7
Sam Bulman <161944461+elemelon202@users.noreply.github.com>
2025-11-18 15:27:41 +0900

    created the story index, commented out the navbar so plz check

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,5 +2,6 @@ class Story < ApplicationRecord
   belongs_to :user
   has_many :chats, dependent: :destroy
   has_many :messages, through: :chats
+  has_one_attached :images
   validates :age, presence: :true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,28 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user = User.first || User.create(email: "testboi@sam.com", password: "123456")
+
+stories = [{
+  name: "Little Red Riding Boot",
+  age: 5,
+  theme: "adventure",
+  system_prompt: "system_prompt"
+}, {
+  name: "Sam in Wonderland",
+  age: 7,
+  theme: "fantasy",
+  system_prompt: "system_prompt"
+}, {
+  name: "Dino's Day Out",
+  age: 3,
+  theme: "cute",
+  system_prompt: "system_prompt"
+}, {
+  name: "Shadows of Eldermoor",
+  age: 14,
+  theme: "dark",
+  system_prompt: "system_prompt"
+}
+]
+
+puts "Cleaning the DB"
+stories.each { |s| Story.create!(s.merge(user: user))}
+puts "Created #{Story.count} stories"
