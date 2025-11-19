65a5a0d7d76faca39b87ee9f1ad19e1fbaefe9bd
Sam Bulman <161944461+elemelon202@users.noreply.github.com>
2025-11-19 15:12:20 +0900

    form working better, chat bot gives title and summary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_18_022811) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_19_052257) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_18_022811) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "summary"
     t.index ["user_id"], name: "index_stories_on_user_id"
   end
 
