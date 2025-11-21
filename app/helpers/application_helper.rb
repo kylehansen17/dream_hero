a30a8d83ffafad271d645b54df091809149c7944
Tyrhen Cameron <tyrhencameron@gmail.com>
2025-11-21 09:35:29 +0900

    button styles and character tab background

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def render_markdown(text)
     Kramdown::Document.new(text, input: 'GFM', syntax_highlighter: "rouge").to_html
   end
+
+  def page_body_class
+    "#{controller_name}-#{action_name}"
+  end
 end
